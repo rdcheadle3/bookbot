a37999ea91aea51af1c9bed5890846591469a82e
rdcheadle3 <robert.cheadle01@gmail.com>
2024-05-05 20:53:43 -0400

    list of dictionaries sorted by hi to lo count

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def main():
+    path = "books/frankenstein.txt"
+    text = read_book(path)
+    count = word_count(text)
+    dictionary = letter_count(text)
+    sorted_list = create_sorted_list(dictionary)
+    print(f"--- Begin report of {path} ---")
+    print(f"{count} words were found in the document" )
+    print()
+    for item in sorted_list:
+        print(item['letter'])
+
 def read_book(path):
     with open(path) as f:
         return f.read()
@@ -10,17 +22,21 @@ def letter_count(text):
     text = text.lower()
     character_dict = {}
     for character in text:
-        if character in character_dict:
-            character_dict[character] += 1
-        else: character_dict[character] = 1
+        if character.isalpha():
+            if character in character_dict:
+                character_dict[character] += 1
+            else: character_dict[character] = 1
     return character_dict
 
-def main():
-    path = "books/frankenstein.txt"
-    text = read_book(path)
-    count = word_count(text)
-    letter_count_dict = letter_count(text)
-    print("-- Begin report of " + path)
-    print("\n %s words were found in the document" % (count))
+def sort_data_by(d):
+    return d['num']
+
+def create_sorted_list(character_dict):
+    sorted_list = []
+    for char in character_dict:
+        sorted_list.append({"letter": char, "num": character_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_data_by)
+    return sorted_list
+    
 
 main()
